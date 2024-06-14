d90620eb8ca823e190ee3ab14bf6e423d5811b32
Goldbar97 <ehrm1357@gmail.com>
2024-06-15 01:16:31 +0900

    refactor: repository queries

--- src/main/java/kang/tableorder/repository/CustomerReviewRepository.java
+++ src/main/java/kang/tableorder/repository/CustomerReviewRepository.java
@@ -3,18 +3,29 @@ package kang.tableorder.repository;
 import java.util.List;
 import java.util.Optional;
 import kang.tableorder.entity.CustomerReviewEntity;
+import kang.tableorder.entity.MenuEntity;
 import kang.tableorder.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerReviewRepository extends JpaRepository<CustomerReviewEntity, Integer> {
 
   Optional<CustomerReviewEntity> findByIdAndUserEntityId(Integer id, Integer userEntityId);
 
+  @Modifying
   @Query("DELETE FROM CUSTOMER_REVIEW cr "
       + "WHERE cr.id = :id "
       + "AND cr.userEntity = :userEntity")
   void deleteByIdAndUserEntity(Integer id, UserEntity userEntity);
 
   List<CustomerReviewEntity> findAllByUserEntityId(Integer userEntityId);
+
+  Optional<CustomerReviewEntity> findTop1ByMenuEntityOrderByCreatedAtDesc(MenuEntity menuEntity);
+
+  Page<CustomerReviewEntity> findAllByMenuEntity(MenuEntity menuEntity, Pageable pageable);
+
+  Optional<CustomerReviewEntity> findByIdAndMenuEntity(Integer id, MenuEntity menuEntity);
 }
---
+++ src/main/java/kang/tableorder/repository/VisitedUsersRepository.java
@@ -0,0 +1,15 @@
+package kang.tableorder.repository;
+
+import java.util.Optional;
+import kang.tableorder.entity.RestaurantEntity;
+import kang.tableorder.entity.UserEntity;
+import kang.tableorder.entity.VisitedUsersEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitedUsersRepository extends JpaRepository<VisitedUsersEntity, Integer> {
+
+  Optional<VisitedUsersEntity> findByUserEntityAndRestaurantEntity(UserEntity userEntity,
+      RestaurantEntity restaurantEntity);
+}
